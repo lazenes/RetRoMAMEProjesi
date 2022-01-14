09bd4efba72d0b34b8d565c6a4fb510e76f14a6b
Enes BİBER <lazenes@gmail.com>
2022-01-15 01:55:26 +0300

    Add files via upload

--- Arcade_C#/ArCadeOtomosyon/Form1.Designer.cs
+++ Arcade_C#/ArCadeOtomosyon/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace ArCadeOtomosyon
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -38,6 +39,11 @@ namespace ArCadeOtomosyon
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
+            this.txtAlinan = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,13 +56,14 @@ namespace ArCadeOtomosyon
             this.button1.TabIndex = 0;
             this.button1.Text = "Makinayı Çalıştır";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(51, 134);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(208, 22);
+            this.progressBar1.Size = new System.Drawing.Size(255, 22);
             this.progressBar1.TabIndex = 1;
             // 
             // pictureBox1
@@ -101,7 +108,7 @@ namespace ArCadeOtomosyon
             // 
             // timer1
             // 
-            this.timer1.Interval = 3000;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label3
@@ -112,13 +119,66 @@ namespace ArCadeOtomosyon
             this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Bağlantı Sinyali:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtAlinan
+            // 
+            this.txtAlinan.BackColor = System.Drawing.SystemColors.MenuText;
+            this.txtAlinan.ForeColor = System.Drawing.Color.Lime;
+            this.txtAlinan.Location = new System.Drawing.Point(12, 230);
+            this.txtAlinan.Multiline = true;
+            this.txtAlinan.Name = "txtAlinan";
+            this.txtAlinan.Size = new System.Drawing.Size(334, 52);
+            this.txtAlinan.TabIndex = 7;
+            this.txtAlinan.TextChanged += new System.EventHandler(this.txtAlinan_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Servis Komut:";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(214, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Gönder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(12, 59);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScriptErrorsSuppressed = true;
+            this.webBrowser2.Size = new System.Drawing.Size(320, 56);
+            this.webBrowser2.TabIndex = 11;
+            this.webBrowser2.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(344, 243);
+            this.ClientSize = new System.Drawing.Size(355, 294);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtAlinan);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -126,8 +186,11 @@ namespace ArCadeOtomosyon
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.webBrowser2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Arcade Otomosyon | Enes BiBER.Com.TR";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -145,6 +208,11 @@ namespace ArCadeOtomosyon
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAlinan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- Arcade_C#/ArCadeOtomosyon/Form1.cs
+++ Arcade_C#/ArCadeOtomosyon/Form1.cs
@@ -12,23 +12,57 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Converters;
 using System.IO;
 using System.Diagnostics;
+using System.Management;
+using System.IO.Ports;
 
 namespace ArCadeOtomosyon
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
-
+       
+        String url = "";
+        int jeton = 0;      
+        SerialPort serialPort1;
+        bool sayfa = false;
         private void Form1_Load(object sender, EventArgs e)
         {
-            var MyIni = new IniFile(Path.Combine(Environment.CurrentDirectory, "ayarlar.ini"));
-
-            string url = MyIni.Read("url", "ayarlar");
-            webBrowser1.Navigate(url);
+            //webBrowser1.Document.Cookie.Remove(0, webBrowser1.Document.Cookie.Length);
            
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+            var MyIni = new IniFile(Path.Combine(Environment.CurrentDirectory, "ayarlar.ini"));
+            webBrowser2.Navigate("https://arcade.enesbiber.com.tr/cmd.php?online=Online&durum=1");
+            url = MyIni.Read("url", "ayarlar");
+            string sonRom = MyIni.Read("SonRom", "ayarlar");
+            string Oyun = MyIni.Read("Oyun", "ayarlar");
+            string port = MyIni.Read("PORT", "ayarlar");
+            serialPort1 = new SerialPort(port, 9600, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SeriPort_VeriOKU);
+            serialPort1.DtrEnable = true;
+            serialPort1.Open();
+            webBrowser1.Navigate(url);           
+            pictureBox1.Visible = true;
+            if (!string.IsNullOrEmpty(sonRom)) {
+                timer1.Start();
+                label2.Text = Oyun;
+                String dizin = Path.Combine(Environment.CurrentDirectory, "mame.exe");
+                foreach (var processKill in Process.GetProcessesByName("mame"))
+                {
+                    processKill.Kill();
+                }
+               
+                Process process = new Process();
+                process.StartInfo.FileName = dizin;
+                process.StartInfo.Arguments = sonRom + ".zip";
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+            }
         }
         public class OyunBilgi
         {
@@ -41,21 +75,6 @@ namespace ArCadeOtomosyon
             public List<OyunBilgi> oyunBilgi { get; set; }
         }
 
-        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            //label2.Text=webBrowser1.Document.Body.InnerText;
-            var  RootObjects = JsonConvert.DeserializeObject<List<OyunBilgi>>(webBrowser1.Document.Body.InnerText);
-
-           
-
-            foreach (var rootObject in RootObjects)
-            {
-                
-                label2.Text = rootObject.isim;
-               
-            }
-            
-        }
 
         private void WebBrowser1_ProgressChanged(Object sender,
                                            WebBrowserProgressChangedEventArgs e)
@@ -69,42 +88,213 @@ namespace ArCadeOtomosyon
 
         private void button1_Click(object sender, EventArgs e)
         {
-            timer1.Start();
+           
             pictureBox1.Visible = true;
+            button1.Visible = false;
           
         }
+        [System.Runtime.InteropServices.DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
 
+        public static bool CheckNet()
+        {
+            int desc;
+            return InternetGetConnectedState(out desc, 0);
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            webBrowser1.Refresh(WebBrowserRefreshOption.Completely);
-            //label2.Text=webBrowser1.Document.Body.InnerText;
-            var RootObjects = JsonConvert.DeserializeObject<List<OyunBilgi>>(webBrowser1.Document.Body.InnerText);
-
-
-
-            foreach (var rootObject in RootObjects)
+            
+            try
             {
+                sayfa = true;
+                webBrowser1.Refresh(WebBrowserRefreshOption.Completely);
+              //  webBrowser1.Navigate(url);
 
-                
-                if (label2.Text != rootObject.isim) { 
-                    
-                    label2.Text = rootObject.isim;
-                    String dizin = Path.Combine(Environment.CurrentDirectory, "mame.exe");
-                    //, strMsgReceive
+               // label3.Text = CheckNet().ToString();
+            if (CheckNet())
+            {
+                    label3.ForeColor = Color.Green;
 
+                    label3.Text = "Bağlantı Sinyali: ONLİNE";
+
+
+                    if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                    {
+
+                        var RootObjects = JsonConvert.DeserializeObject<List<OyunBilgi>>(webBrowser1.Document.Body.InnerText);
+
+                        foreach (var rootObject in RootObjects)
+                        {
+
+                            var MyIni = new IniFile(Path.Combine(Environment.CurrentDirectory, "ayarlar.ini"));
+                            string Oyun = MyIni.Read("Oyun", "ayarlar");
+                            if (Oyun != rootObject.isim)
+                            {
+
+                                label2.Text = rootObject.isim;
+                                String dizin = Path.Combine(Environment.CurrentDirectory, "mame.exe");
+                                if (rootObject.dosya != "kapat" && rootObject.dosya != "Arcade_Kapat" && rootObject.dosya != "Restart" && rootObject.dosya != "jeton" && rootObject.dosya != "dur")
+                                {
+                                    foreach (var processKill in Process.GetProcessesByName("mame"))
+                                    {
+                                        processKill.Kill();
+                                    }
+
+                                    MyIni.Write("SonRom", rootObject.dosya, "ayarlar");
+                                    MyIni.Write("Oyun", rootObject.isim, "ayarlar");
+
+                                    txtAlinan.Text = "\n" + txtAlinan.Text + rootObject.isim + "\n";
+                                    Process process = new Process();
+                                    process.StartInfo.FileName = dizin;
+                                    process.StartInfo.Arguments = rootObject.dosya + ".zip";
+                                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                                    process.StartInfo.UseShellExecute = false;
+                                    process.Start();
+                                }
+
+                                else
+                                {
+                                    if (rootObject.dosya == "jeton")
+                                    {
+                                        jeton++;
+
+                                        ServisMod(rootObject.dosya);
+                                        webBrowser2.Navigate("https://arcade.enesbiber.com.tr/cmd.php?dosya=dur&isim=Servis Modu jeton");
+                                    }
+                                    else
+                                    {
+                                        jeton = 0;
+                                        ServisMod(rootObject.dosya);
+                                    }
+                                }
+
+                            }
+                        }
+                    } 
+            
+                }
+                else
+                {
+                    label3.ForeColor = Color.Red;
+                    label3.Text = "Bağlantı Sinyali: OFFLİNE";
+
+
+                }
+            } 
+            catch (NullReferenceException err)
+            {
+                Console.WriteLine("Hiçbir Bir şey olmasa da birşeyler olabilir");
+                Console.WriteLine(err.Message);
+            }
+        }
+         void ServisMod(String komut) {
+
+            switch (komut)
+            {
+                case    "kapat":
+                    // Mame Uygulaması Kapatılır
                     foreach (var processKill in Process.GetProcessesByName("mame"))
                     {
                         processKill.Kill();
                     }
-                    Process process = new Process();
-                    process.StartInfo.FileName = dizin;
-                    process.StartInfo.Arguments = rootObject.dosya + ".zip";
-                    process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    process.StartInfo.UseShellExecute = false;
-                    process.Start();
-                }
+                    break;
+                case "Arcade_Kapat":
+                    //Bilgisayarı kapatır
+                    webBrowser2.Navigate("https://arcade.enesbiber.com.tr/cmd.php?dosya=dur&isim=Servis Modu Kapat");
+                    ArcadeOfflineMOd();
+                    Shutdown(1);
+                    break;
+                case "Restart":
+                    // bilgisayarı yeniden başlatır
+                    webBrowser2.Navigate("https://arcade.enesbiber.com.tr/cmd.php?dosya=dur&isim=Servis Modu Restart");
+                    ArcadeOfflineMOd();
+                    Shutdown(2);
+                    break;
+                case "jeton":
+                    // oyuna jeton atar
+                    serialPort1.Write("jeton");
+                   
+                        break;
+                default:
+                    // code block
+                    
+                    break;
+            }
+        }
+     
+
+void Shutdown(int flg)
+    {
+        ManagementBaseObject mboShutdown = null;
+        ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+        mcWin32.Get();
+
+        // You can't shutdown without security privileges
+        mcWin32.Scope.Options.EnablePrivileges = true;
+        ManagementBaseObject mboShutdownParams =
+                 mcWin32.GetMethodParameters("Win32Shutdown");
+
+        // Flag 1 means we want to shut down the system. Use "2" to reboot.
+        mboShutdownParams["Flags"] = flg;
+        mboShutdownParams["Reserved"] = "0";
+        foreach (ManagementObject manObj in mcWin32.GetInstances())
+        {
+            mboShutdown = manObj.InvokeMethod("Win32Shutdown",
+                                           mboShutdownParams, null);
+        }
+    }
+
+        private void SeriPort_VeriOKU(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+           // SeriPortVeri = serialPort1.ReadExisting();
+           txtAlinan.Text = "\n"+ txtAlinan.Text + serialPort1.ReadExisting() + "\n";
+           txtAlinan.Select(txtAlinan.Text.Length, 0);
+            // MessageBox.Show("vERİ gELDİ");
+           
+        }
+
+        private void txtAlinan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(textBox1.Text);
+        }
+     
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                ArcadeOfflineMOd();
             }
            
+
+
+        }
+        void ArcadeOfflineMOd() {
+
+            String dizin = Path.Combine(Environment.CurrentDirectory, "offline.exe");
+
+            Process process = new Process();
+            process.StartInfo.FileName = dizin;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            sayfa = true;
+            timer1.Start();
         }
     }
 }
